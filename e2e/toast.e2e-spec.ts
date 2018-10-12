8d52f3626744544e5c1f51c3d0933f711d364bd2
Conor Wright <conor.wright@blackbaud.com>
2018-10-12 11:55:33 -0400

    Initial port (#1)

---
+++ e2e/toast.e2e-spec.ts
@@ -0,0 +1,47 @@
+import {
+  expect,
+  SkyHostBrowser
+} from '@skyux-sdk/e2e';
+
+import {
+  element,
+  by
+} from 'protractor';
+
+describe('Toast', () => {
+  beforeEach(() => {
+    SkyHostBrowser.get('visual/toast');
+  });
+
+  it('should match previous toast screenshot', (done) => {
+    SkyHostBrowser.setWindowBreakpoint('lg');
+    element(by.css('.sky-btn-primary')).click();
+    expect('.sky-toaster').toMatchBaselineScreenshot(done, {
+      screenshotName: 'toast-lg'
+    });
+  });
+
+  it('should match previous templated toast screenshot', (done) => {
+    SkyHostBrowser.setWindowBreakpoint('lg');
+    element(by.css('.sky-btn-secondary')).click();
+    expect('.sky-toaster').toMatchBaselineScreenshot(done, {
+      screenshotName: 'toast-lg-component'
+    });
+  });
+
+  it('should match previous toast screenshot on tiny screens', (done) => {
+    SkyHostBrowser.setWindowBreakpoint('xs');
+    element(by.css('.sky-btn-primary')).click();
+    expect('.sky-toaster').toMatchBaselineScreenshot(done, {
+      screenshotName: 'toast-xs'
+    });
+  });
+
+  it('should match previous templated toast screenshot on tiny screens', (done) => {
+    SkyHostBrowser.setWindowBreakpoint('xs');
+    element(by.css('.sky-btn-secondary')).click();
+    expect('.sky-toaster').toMatchBaselineScreenshot(done, {
+      screenshotName: 'toast-xs-component'
+    });
+  });
+});

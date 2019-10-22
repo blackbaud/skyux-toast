365f9bd78de279914b03d64adb3d2d4fb2761d8a
Paul Crowder <paul.crowder@blackbaud.com>
2019-10-22 14:34:14 -0400

    Fix import formatting

--- src/app/public/modules/toast/toaster.service.ts
+++ src/app/public/modules/toast/toaster.service.ts
@@ -1,7 +1,10 @@
 import {
   Injectable
 } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import {
+  BehaviorSubject
+} from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class SkyToasterService {

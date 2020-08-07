8332f1cb8243d35af63fa75af17a620389e3c915
John Lyons <John.Lyons@blackbaud.me>
2020-08-07 17:17:12 -0400

    Set up docs tools

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -7,6 +7,15 @@ import {
 } from '@angular/platform-browser/animations';
 
 import {
+  SkyCodeModule
+} from '@blackbaud/skyux-lib-code-block';
+
+import {
+  SkyDocsToolsModule,
+  SkyDocsToolsOptions
+} from '@skyux/docs-tools';
+
+import {
   SkyAppLinkModule
 } from '@skyux/router';
 
@@ -25,10 +34,21 @@ import {
   ],
   exports: [
     SkyAppLinkModule,
+    SkyCodeModule,
+    SkyDocsToolsModule,
     SkyToastModule
   ],
   entryComponents: [
     ToastDemoComponent
+  ],
+  providers: [
+    {
+      provide: SkyDocsToolsOptions,
+      useValue: {
+        gitRepoUrl: 'https://github.com/blackbaud/skyux-popovers',
+        packageName: '@skyux/toast'
+      }
+    }
   ]
 })
 export class AppExtrasModule { }

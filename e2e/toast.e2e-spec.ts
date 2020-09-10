25130c798f7fc7abfb0e9f707456bb6548f57bbe
Paul Crowder <paul.crowder@blackbaud.com>
2020-09-10 10:10:50 -0400

    Added modern theme style to toast (#57)
    * Added modern theme style to toast
    
    * Fixed property order
    
    Co-authored-by: paulisme <paul@paulcrowder.net>

--- e2e/toast.e2e-spec.ts
+++ e2e/toast.e2e-spec.ts
@@ -1,6 +1,7 @@
 import {
   expect,
-  SkyHostBrowser
+  SkyHostBrowser,
+  SkyVisualThemeSelector
 } from '@skyux-sdk/e2e';
 
 import {
@@ -9,41 +10,93 @@ import {
 } from 'protractor';
 
 describe('Toast', () => {
+  let currentTheme: string;
+  let currentThemeMode: string;
 
-  beforeEach(() => {
-    SkyHostBrowser.get('visual/toast');
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
+
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
+
+  function getScreenshotName(name: string): string {
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
+
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function runTests(): void {
+    it('should match previous toast screenshot', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('lg');
+
+      await element(by.css('.sky-btn-primary')).click();
+
+      expect('.sky-toaster').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('toast-lg')
+      });
+    });
+
+    it('should match previous templated toast screenshot', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('lg');
+
+      await element(by.css('.sky-btn-secondary')).click();
+
+      expect('.sky-toaster').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('toast-lg-component')
+      });
+    });
+
+    it('should match previous toast screenshot on tiny screens', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('xs');
+
+      await element(by.css('.sky-btn-primary')).click();
+
+      expect('.sky-toaster').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('toast-xs')
+      });
+    });
+
+    it('should match previous templated toast screenshot on tiny screens', async (done) => {
+      await SkyHostBrowser.setWindowBreakpoint('xs');
+
+      await element(by.css('.sky-btn-secondary')).click();
+
+      expect('.sky-toaster').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('toast-xs-component')
+      });
+    });
+  }
+
+  beforeEach(async () => {
+    currentTheme = undefined;
+    currentThemeMode = undefined;
+
+    await SkyHostBrowser.get('visual/toast');
   });
 
-  it('should match previous toast screenshot', (done) => {
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    element(by.css('.sky-btn-primary')).click();
-    expect('.sky-toaster').toMatchBaselineScreenshot(done, {
-      screenshotName: 'toast-lg'
+  runTests();
+
+  describe('when modern theme', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'light');
     });
+
+    runTests();
   });
 
-  it('should match previous templated toast screenshot', (done) => {
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    element(by.css('.sky-btn-secondary')).click();
-    expect('.sky-toaster').toMatchBaselineScreenshot(done, {
-      screenshotName: 'toast-lg-component'
+  describe('when modern theme in dark mode', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'dark');
     });
-  });
 
-  it('should match previous toast screenshot on tiny screens', (done) => {
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    element(by.css('.sky-btn-primary')).click();
-    expect('.sky-toaster').toMatchBaselineScreenshot(done, {
-      screenshotName: 'toast-xs'
-    });
-  });
-
-  it('should match previous templated toast screenshot on tiny screens', (done) => {
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    element(by.css('.sky-btn-secondary')).click();
-    expect('.sky-toaster').toMatchBaselineScreenshot(done, {
-      screenshotName: 'toast-xs-component'
-    });
+    runTests();
   });
 
 });

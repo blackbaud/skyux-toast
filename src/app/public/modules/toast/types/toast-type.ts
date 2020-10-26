224cb9b353a86d43e8c5f1b1dcccbac9a0de775b
John Lyons <John.Lyons@blackbaud.me>
2020-10-26 09:05:40 -0400

    Fix spacing for JSDocs asterisks

--- src/app/public/modules/toast/types/toast-type.ts
+++ src/app/public/modules/toast/types/toast-type.ts
@@ -1,18 +1,18 @@
 export enum SkyToastType {
   /**
-  * Sets the toast's icon and background color to the `danger` style.
-  */
+   * Sets the toast's icon and background color to the `danger` style.
+   */
   Danger,
   /**
-  * Sets the toast's icon and background color to the `info` style.
-  */
+   * Sets the toast's icon and background color to the `info` style.
+   */
   Info,
   /**
-  * Sets the toast's icon and background color to the `success` style.
-  */
+   * Sets the toast's icon and background color to the `success` style.
+   */
   Success,
   /**
-  * Sets the toast's icon and background color to the `warning` style.
-  */
+   * Sets the toast's icon and background color to the `warning` style.
+   */
   Warning
 }
